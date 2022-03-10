b1ff87e37c5d740624e853e954782ea6d50a4954
Johnathan Shoff <w.m.t.b5115@gmail.com>
2022-03-10 12:25:28 -0500

    added some extra setup configurations

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const server = express();
+
+const port = 9000;
+const host = '127.0.0.1';
+
+server.use(express.json());
+
+server.get('/', (req, res) =>{
+  res.status(200).send('<h1>Server is running:</h1>')
+});
+
+
+server.listen(port, host, () =>{
+  console.log(`Server is running on port: ${port}`);
+});
